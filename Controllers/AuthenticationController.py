3c8399c691991a0e1d8dc1f53897480b9f3b6397
Khoa Tran <kttran2320@gmail.com>
2019-10-21 16:28:44 -0400

    signup feature

--- Controllers/AuthenticationController.py
+++ Controllers/AuthenticationController.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
+from mongoengine import ValidationError
 from flask import current_app as app
 from Services.AuthenticationService import AuthenticationService
 from Models.User import User
@@ -6,38 +7,37 @@ AuthenticationService = AuthenticationService()
 
 auth = Blueprint("AuthenticationController", __name__, url_prefix="/auth")
 
+
 @auth.route("/login", methods=["POST"])
 def login():
     if not AuthenticationService.authenticate(request.form["email"], request.form["password"]):
         return "Incorrect username or password"
     else:
         return "Authentication"
+
+
 """
 TODO: KOAH
 Create signup API endpoint
 Request params: first name, last name, email (will be used as username), password
 Return: Success or failure codes    
 """
-@auth.route("/signup", methods=["POST"])
+@auth.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
-        # document = {"firstName": request.form["firstName"],
-        #             "lastName": request.form["lastName"],
-        #             "email": request.form["email"],
-        #             "password": request.form["password"]
-        #             }
-        # doc = Document(values=document)
+
         user = User(
             firstName=request.form["firstName"], lastName=request.form["lastName"], email=request.form["email"],
             password=request.form["password"])
-        # try:
-        # if user.validate():
-        # save user to database
-        # try:
-        if user.validate() != "":
+
+        try:
+            user.validate()
             return "It DOES work"
-        # except:
-        #     return "error"
+            # else:
+            #     return "error"
+            #     print(validation)
+        except ValidationError:
+            return "error"
 
     else:
         return "Hello"
