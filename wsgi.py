6b522872c5c0b8bfdf76eeee38edcae4812186b8
Sudo Whodo <csprojects@dhcp-wifi-8021x-155-41-88-42.bu.edu>
2019-10-31 14:11:03 -0400

    added CORS to wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
 import app
 
+from flask_cors import CORS
+
+#CORS for the backend server and frontend to communicate with each other
+app.config['SECRET_KEY'] = 'secret'
+CORS(app)
+
 app = app.create_app()
+
+
 
 if __name__ == "main":
     app.run()
