0d66e0ece8a4c60fa91ec734fe8588d5667d72b7
Conor Walsh <cewalsh@bu.edu>
2019-11-11 15:14:39 -0500

    added changePassword and changeEmail methods

--- Services/AuthenticationService.py
+++ Services/AuthenticationService.py
@@ -100,3 +100,11 @@ class AuthenticationService():
             success = False
         
         return success, error
+
+    def changeEmail(self, oldEmail, newEmail):
+            User.objects.get(email=oldEmail).update(email=newEmail)
+
+    def changePassword(self, email, password):
+            User.objects.get(email=email).update(password=self.saltPassword(password))   
+
+            
