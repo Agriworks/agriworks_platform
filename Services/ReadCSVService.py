ceda75359b08f35fd10fe5f9a76858ea3378ed15
Cody <cody@dhcp-wifi-8021x-155-41-28-80.bu.edu>
2019-11-13 18:27:14 -0500

    moved pandas import statement

---
+++ Services/ReadCSVService.py
@@ -0,0 +1,4 @@
+from flask import current_app as app
+from app import db
+import pandas as pd
+from pandas import DataFrame, read_csv
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from mongoengine import connect
-import pandas as pd
-from pandas import DataFrame, read_csv
+
 
 
 db = connect('agriworks')
