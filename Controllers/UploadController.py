6ff1324ae76685d0b49380b3e5072ab5c2da7549
Keshav <km02@bu.edu>
2020-02-17 01:41:56 -0500

    Revert "non-working thread code"
    This reverts commit f761a1d89987cbcf4777010cedc923e5fb7e7489.

--- Controllers/UploadController.py
+++ Controllers/UploadController.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, flash, request, redirect, jsonify
-from threading import Thread
+from flask import Blueprint, flash, request, redirect
 from Response import Response
 from flask import current_app as app
 from Services.UploadService import UploadService
@@ -17,11 +16,6 @@ def uploadNewFile():
         if (not UploadService.allowed_file(request.files["file"].filename)):
             return Response("Prohibited file type", status=400) #TODO: Append to response: Dynamically return the types of allowed files
         
-        #check file size
-        if len(request.files['file'].read()) > 1:#0 * 1024 * 1024: # if larger than 10MB
-            dataset = UploadService.threadCreateDataSetAndDataObjects(request)
-            return jsonify({'started': True})
-
         dataset = UploadService.createDataSetAndDataObjects(request)
         
         return Response(str(dataset.id))
--- Services/UploadService.py
+++ Services/UploadService.py
@@ -7,7 +7,6 @@ from mongoengine import ValidationError
 import pandas as pd
 import numpy
 import boto3
-from uwsgidecorators import thread
 
 AuthenticationService = AuthenticationService()
 
@@ -29,81 +28,6 @@ class UploadService():
     #want to process the error and return the appropriate error message.
     #TODO: Verify that the user that is uploading this dataset is logged in. 
     def createDataSetAndDataObjects(self, request):
-        try:
-            user = AuthenticationService.verifySessionAndReturnUser(request.cookies["SID"])
-
-            if (not user):
-                return {"message": "Invalid session", "status": 400}
-
-            #TODO: verify that these parameters exist
-            uploadedFile = request.files['file']
-            dataSetName = request.form.get("name")
-            dataSetAuthor = user
-            dataSetIsPublic = True if request.form.get("permissions") == "Public" else False
-            dataSetTags = request.form.get("tags")
-            dataSetType = request.form.get("type")
-
-            #Read the data in 
-            data = pd.read_csv(uploadedFile)
-            keys = list(data.columns)
-            legend = {} #contains same column values
-            nonRepeatedKeys = []
-
-            #Verify that no empty fields exist. 
-            if (data.isnull().values.sum() > 0):
-                raise ValueError
-
-            #Combine repeated column data into legend
-            for i in keys:
-                if len(data[i]) <= 1: #if only one row of values, then quit
-                    break
-                if data[i].nunique() == 1: #if all entries have the same value
-                    if str(data[i][0]) != 'nan': #checking to make sure value in not nan
-                        legend[i] = data[i][0]
-                    if isinstance(data[i][0], numpy.int64):
-                        legend[i] = int(data[i][0])
-                else:
-                    #TODO: Optimize
-                    currentNonRepeatedKeyIndex = keys.index(i)
-                    nonRepeatedKeys.append(keys[currentNonRepeatedKeyIndex])                    
-
-            #Create dataset object
-            dataSet = Dataset(
-                name=dataSetName,
-                author=dataSetAuthor,
-                keys=nonRepeatedKeys,
-                legend=legend,
-                public=dataSetIsPublic,
-                tags=dataSetTags,
-                datasetType=dataSetType
-            )
-            dataSet.save()
-
-            #Populate data into database 
-            for i in range(len(data)):
-                dataObject = DataObject(dataSetId=dataSet)
-                for j in range(len(keys)):
-                    currentItem = data.iloc[i][j]
-                    if (keys[j] not in legend):                      
-                        if (isinstance(currentItem, numpy.int64)):
-                            currentItem = int(currentItem) #cast int64 objects to ints 
-                        dataObject[keys[j]] = currentItem
-                dataObject.save()
-
-            #Go back to the front of the file
-            uploadedFile.seek(0)
-
-            #Save to S3
-            self.uploadToAWS(dataSet.id, uploadedFile)
-
-            return dataSet #TODO: How do we automatically get a string rep of a mongo object id ?
-        
-        except ValidationError as e:
-            print(e)
-            return None
-
-    @thread
-    def threadCreateDataSetAndDataObjects(self, request):
         try:
             user = AuthenticationService.verifySessionAndReturnUser(request.cookies["SID"])
 
