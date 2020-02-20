d49a3432bb8a0589c4f084b0507166d6de69f1d4
Ning <sning.wang@outlook.com>
2020-02-19 19:38:12 -0500

    can now return json in response

--- Response.py
+++ Response.py
@@ -16,10 +16,16 @@ class Response(FlaskResponse):
 
         if (isinstance(response, str)):
             response = json.dumps({"message": response})
-        
+
+        if (isinstance(response, list)):
+            response = json.dumps(response)
+
         if (isinstance(response, dict) and "status" in response and isinstance(response["status"], int)): 
             kwargs["status"] = response["status"]
             response.pop("status")
+        
+        if (isinstance(response, dict)):
+            response = json.dumps(response)
 
         if("content_type" not in kwargs):
             kwargs["content_type"] = "application/json"
