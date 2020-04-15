cf942672204c65a6b9bf332405cd23fabd291a13
Keshav <km02@bu.edu>
2020-04-15 01:29:40 -0400

    changed var to msg

--- Controllers/AuthenticationController.py
+++ Controllers/AuthenticationController.py
@@ -61,8 +61,8 @@ def signup():
     userConfirmationId = uuid4()
     user = User.objects.get(email=user["email"])
     AuthenticationService.setUserConfirmationId(user, userConfirmationId)
-    emailConfirmationMessage = "<p>Congratulations, you just got registered on Agriworks. Please click the link below to confirm your account.</p><p><a href=\"http://agri-works.org/confirm-user/{0}\">http://agri-works.org/confirm-user/{0}</a></p><br/>".format(userConfirmationId)
-    MailService.sendMessage(user, "[Agriworks] Confirm Account", emailConfirmationMessage)
+    msg = "<p>Congratulations, you just got registered on Agriworks. Please click the link below to confirm your account.</p><p><a href=\"http://agri-works.org/confirm-user/{0}\">http://agri-works.org/confirm-user/{0}</a></p><br/>".format(userConfirmationId)
+    MailService.sendMessage(user, "[Agriworks] Confirm Account", msg)
     return Response("Signup successful", status=200)
 
 @auth.route("/confirm-user/<userConfirmationId>", methods=["POST"])
