06a51f2543d6945e102b15714bc79c23e2552d8e
Cody Clark-Singh <cody@sigres.com>
2020-04-22 18:51:22 -0400

    done

--- Controllers/AuthenticationController.py
+++ Controllers/AuthenticationController.py
@@ -79,7 +79,7 @@ def resendConfirmationEmail(email):
         sub = "Confirm Account"
         msg = f"<p>Congratulations, you've registered for Agriworks. Please click the link below to confirm your account.</p><p><a href=\"{app.rootUrl}/confirm-user/{userConfirmationId2}\"> Confirm account </a></p>"
         MailService.sendMessage(user, sub, msg)
-        return Response("Confirmation email sent", status=200)
+        return Response("New confirmation email sent.", status=200)
     except:
         return Response("Resend confirmation email unsuccessful.", status=403)
 
