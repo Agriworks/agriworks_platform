a0b0cd205c6fd9958f31cf2eab1ba361d31a215a
Keshav Maheshwari <km02@bu.edu>
2020-07-27 17:55:17 -0400

    removed placeholder from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Testing circleci deploy pipeline
+
 #START IN PLATFORM ROOT DIR. PORTAL ROOT DIR MUST BE LOCATED IN PLATFORM'S PARENT DIR.
 cd ../agriworks_portal && yarn build
 cd ../agriworks_platform && eb deploy
