c7fac61141b1d110fef73817b951f4882501917c
Cody Clark-Singh <cody@sigres.com>
2020-09-09 14:43:24 -0400

    complete basic class structure

--- Services/LocationRelationshipService.py
+++ Services/LocationRelationshipService.py
@@ -2,9 +2,28 @@ import csv
 
 class LocationRelationshipService():
     def __init__(self):
+        # TODO: update version functionality
+        self.version = None
         # Load location relationships onto memory upon initization
+        villageToDistrictDic = {}
+        districtToStateDic = {}
         with open("../LocationRelationships.csv") as csvfile:
-            readCSV = csv.reader(csvfile, delimiter=",")
-            for row in readCSV:
-                print(row)
+            csvreader = csv.reader(csvfile, delimiter=",")
+            csvreader.next()
+            for row in csvreader:
+                villageToDistrictDic[row[2]] = row[1]
+                districtToStateDic[row[1]] = row[0]
+        self.villageToDistrictDic = villageToDistrictDic
+        self.districtToStateDic = districtToStateDic
         return
+
+    def villageToDistrict(self,village):
+        return self.villageToDistrictDic[village]
+
+    def districtToState(self,district):
+        return self.districtToStateDic[district]
+
+    def getVersion(self):
+        return self.version
+
+    
