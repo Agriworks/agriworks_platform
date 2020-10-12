9c8893fb21704dcdc0cb046465986feec707a9dd
Conor <cewalsh@bu.edu>
2020-10-12 16:15:49 -0400

    Started matching a coordinate to a feature in the json

--- Services/VisualizeService.py
+++ Services/VisualizeService.py
@@ -1,5 +1,7 @@
 import secrets
 import json
+from shapely.geometry import shape, GeometryCollection, Point
+
 
 class VisualizeService():
     def __init__(self):
@@ -50,7 +52,11 @@ class VisualizeService():
         return datacollection
 
 
-    def getMap(self, dataset, name_col, data_col):
+    def getMap(self, dataset, loc_col, data_col):
+
+        checkName = False
+
+        print("In the getMap Function")
 
         #the geojson file with the borders, the shape file
         with open("Services/US_States.json", "r") as read_file:
@@ -59,7 +65,7 @@ class VisualizeService():
         #assign the color fill to each line
         colors =[(237,248,233), (186,228,179), (116,196,118), (49,163,84), (0,109,44)]
         numColors = len(colors)
-        low = int(dataset[0][data_col])
+        low = int(dataset[0] [data_col])
         high = int(dataset[0][data_col])
 
         for line in dataset:
@@ -67,19 +73,42 @@ class VisualizeService():
             high = max(x, high)
             low = min(x,low)
 
-
         bucketSize = (high - low)/numColors
 
-        for line in area["features"]:
-            name = line["properties"]["NAME"]
+        if checkName:
+            for line in area["features"]:
+                name = line["properties"]["NAME"]
+                for dataset_line in dataset:
+                    if name == dataset_line[loc_col]:
+                        num = int(dataset_line[data_col])
+                        line["properties"]["data"] = num
+                        bucketNum = int((num - low)//bucketSize)
+                        line["properties"]["color"] = colors[bucketNum]
+                        break
+
+        else: #using location coordinates
+            #first loop through all the features and add data and color properties
+            print("About to go through features")
+            for line in area["features"]:
+                num = 0
+                line["properties"]["data"] = num
+                bucketNum = int((num - low)//bucketSize)
+                line["properties"]["color"] = colors[bucketNum]
+
+            #then loop through the dataset, I did the double loop this way because if the location from the dataset matches a feature, then you can break loop
             for dataset_line in dataset:
-                if name == dataset_line[name_col]:
-                    num = int(dataset_line[data_col])
-                    line["properties"]["data"] = num
-                    bucketNum = int((num - low)//bucketSize)
-                    line["properties"]["color"] = colors[bucketNum]
-                    break
-        
+                loc = dataset_line[loc_col]
+                num = int(dataset_line[data_col])
+                #convert the location to a point
+                result = [x.strip() for x in loc.split(',')]
+                point = Point(result[0], result[1])
+                print(point)
+                for feature in area["features"]:
+                    polygon = shape(feature['geometry'])
+
+                    if polygon.contains(point):
+                        print("Found where it belongs")
+            
         bucketGrades = []
         for i in range(numColors):
             bucketGrades.append(int(low + i * bucketSize))
