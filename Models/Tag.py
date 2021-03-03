38277ae98ae52f0b2c894aa2220a302eaced70e7
Quang Nguyen <nddq@bu.edu>
2021-03-03 14:29:32 +0700

    tags working again

--- Models/Tag.py
+++ Models/Tag.py
@@ -2,4 +2,5 @@ from mongoengine import Document, StringField, IntField
 
 class Tag(Document):
     name = StringField(required=True)
+    datasetType = StringField(required=True)
     noOfEntries = IntField(required=True, default=1)
